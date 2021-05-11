696d6f7093f04fcb8145528a003e5730ab501b3f
ykutaykuta <ykutaykuta@gmail.com>
2021-05-11 16:04:43 +0700

    fix compilation with GCC 10

--- trunk/3rdparty/libsrtp-2-fit/crypto/math/datatypes.c
+++ trunk/3rdparty/libsrtp-2-fit/crypto/math/datatypes.c
@@ -79,7 +79,8 @@ int octet_get_weight(uint8_t octet)
 
 /* the value MAX_PRINT_STRING_LEN is defined in datatypes.h */
 
-char bit_string[MAX_PRINT_STRING_LEN];
+// char bit_string[MAX_PRINT_STRING_LEN];
+static char bit_string[MAX_PRINT_STRING_LEN]; // set static variable to fix compilation with GCC 10
 
 uint8_t srtp_nibble_to_hex_char(uint8_t nibble)
 {
